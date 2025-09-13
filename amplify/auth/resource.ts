c352d27244ae244768f11dc95009a1d282526e22
Mike <nonimporta@nonimporta.com>
2025-09-13 11:37:03 +0200

    added amplify

---
+++ amplify/auth/resource.ts
@@ -0,0 +1,12 @@
+import { defineAuth } from "@aws-amplify/backend";
+
+export const auth = defineAuth({
+  loginWith: {
+    email: {
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "Welcome to the AI-Powered Recipe Generator!",
+      verificationEmailBody: (createCode) =>
+        `Use this code to confirm your account: ${createCode()}`,
+    },
+  },
+});
